d98ae5a7ce98d2715f7707c96ad26d33e2c13a8a
TALIB KHAN <talibasus90@gmail.com>
2026-06-11 12:18:34 +0530

    update

--- 3_RTOS_edf_rms_proportional.c
+++ 3_RTOS_edf_rms_proportional.c
@@ -1,106 +1,168 @@
-#include<stdio.h>
+#include <stdio.h>
 
-struct process{
+#define MAX 100
+
+struct Process
+{
     int id;
     int bt;
-    int deadline;
-    int share;
-    int period;
+    int deadline; // EDF
+    int period;   // RMS
+    int share;    // Proportional Share
     int ct, wt, tat;
 };
 
-
-void reset(struct process p[], int n){
-    for(int i=0; i<n; i++){
-        p[i].tat = p[i].ct = p[i].wt =0;
-    }
+void resetProcesses(struct Process p[], int n)
+{
+    for (int i = 0; i < n; i++)
+        p[i].ct = p[i].wt = p[i].tat = 0;
 }
 
+// ---------- EDF ----------
+void EDF(struct Process p[], int n)
+{
+    printf("\n===== Earliest Deadline First (EDF) =====\n");
 
-// EDF
-void edf(struct process p[], int n){
-    // sort by deadline
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if (p[j].deadline > p[j+1].deadline){
-                struct process temp = p[j];
-                p[j] = p[j+1];
-                p[j+1] = temp;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (p[j].deadline > p[j + 1].deadline)
+            {
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
-        }
-
-    }
 
     int time = 0;
-    for(int i=0; i<n; i++){
-        p[i].wt = time;;
-        time +=p[i].bt;
+
+    for (int i = 0; i < n; i++)
+    {
+        p[i].wt = time;
+        time += p[i].bt;
         p[i].ct = time;
         p[i].tat = p[i].ct;
     }
 
-    printf("ID Deadline WT BT CT TAT"); //loop chalao
+    printf("ID\tBT\tDeadline\tCT\tWT\tTAT\n");
+
+    for (int i = 0; i < n; i++)
+        printf("%d\t%d\t%d\t\t%d\t%d\t%d\n",
+               p[i].id, p[i].bt, p[i].deadline,
+               p[i].ct, p[i].wt, p[i].tat);
 }
 
+// ---------- RMS ----------
+void RMS(struct Process p[], int n)
+{
+    printf("\n===== Rate Monotonic Scheduling (RMS) =====\n");
 
-
-// RMS - rate monotonic scheduling
-
-void edf(struct process p[], int n){
-    // sort by period
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if (p[j].period > p[j+1].period){
-                struct process temp = p[j];
-                p[j] = p[j+1];
-                p[j+1] = temp;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (p[j].period > p[j + 1].period)
+            {
+                struct Process temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
-        }
-
-    }
 
     int time = 0;
-    for(int i=0; i<n; i++){
-        p[i].wt = time;;
-        time +=p[i].bt;
+
+    for (int i = 0; i < n; i++)
+    {
+        p[i].wt = time;
+        time += p[i].bt;
         p[i].ct = time;
         p[i].tat = p[i].ct;
     }
 
-    printf("ID Period WT BT CT TAT"); //loop chalao
+    printf("ID\tBT\tPeriod\tCT\tWT\tTAT\n");
+
+    for (int i = 0; i < n; i++)
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].id, p[i].bt, p[i].period,
+               p[i].ct, p[i].wt, p[i].tat);
 }
 
-// Proportional sharing
- void proportional(struct process p[], int n){
-    int total_bt = 0, total_share=0;
+// ---------- Proportional Share ----------
+void ProportionalShare(struct Process p[], int n)
+{
+    printf("\n===== Proportional Share Scheduling =====\n");
 
-    for(int i=0; i<n; i++){
-        total_bt +=p[i].bt;
-        total_share = p[i].share;
-    }
+    int remaining[MAX];
 
-    int time =0, rem[20];
+    for (int i = 0; i < n; i++)
+        remaining[i] = p[i].bt;
 
-    int completed = 0;
-    while(completed <n){
+    int time = 0, done = 0;
 
-        for(int i=0; i<n; i++){
-            if(rem[i] >0){
-                int timeslice = 0;
-                timeslice = (p[i].share >0) ? 1: 0;
-                if(rem[i] <timeslice) timeslice = rem[i];
-                time +=timeslice;
-                rem[i] -=timeslice;
+    while (done < n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (remaining[i] > 0)
+            {
+                int timeSlice = 1;
 
-                if (rem[i] ==0){
+                if (remaining[i] < timeSlice)
+                    timeSlice = remaining[i];
+
+                time += timeSlice;
+                remaining[i] -= timeSlice;
+
+                if (remaining[i] == 0)
+                {
                     p[i].ct = time;
                     p[i].tat = p[i].ct;
                     p[i].wt = p[i].tat - p[i].bt;
-                    completed++;
+                    done++;
                 }
             }
         }
     }
- }
+
+    printf("ID\tBT\tShare\tCT\tWT\tTAT\n");
+
+    for (int i = 0; i < n; i++)
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].id, p[i].bt, p[i].share,
+               p[i].ct, p[i].wt, p[i].tat);
+}
+
+// ---------- Main ----------
+int main()
+{
+    int n;
+    struct Process p[MAX];
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        p[i].id = i + 1;
+
+        printf("\nProcess %d\n", i + 1);
+
+        printf("Burst Time: ");
+        scanf("%d", &p[i].bt);
+
+        printf("Deadline (EDF): ");
+        scanf("%d", &p[i].deadline);
+
+        printf("Period (RMS): ");
+        scanf("%d", &p[i].period);
+
+        printf("Share (Proportional): ");
+        scanf("%d", &p[i].share);
+    }
+
+    resetProcesses(p, n);
+    EDF(p, n);
+
+    resetProcesses(p, n);
+    RMS(p, n);
+
+    resetProcesses(p, n);
+    ProportionalShare(p, n);
+
+    return 0;
+}
